df619f0ea345973f146e2553e5eb8ca7ca968cd1
nianzhilian <776440949@qq.com>
2025-06-06 11:04:02 +0800

    '提交'

--- src/index.js
+++ src/index.js
@@ -38,7 +38,7 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 //deashboard页面 和 home页面
 //通过路由去匹配组件 从而实现不刷新页面的情况下就能实现页面的切换
 
-function Dashboard(props) {
+/*function Dashboard(props) {
   console.log(props);
   return <div>dashboard页面</div>;
 }
@@ -91,7 +91,35 @@ function App() {
       </Router>
     </>
   );
+}*/
+
+function Input(props,ref){
+  console.log(props)
+  return (
+    <input ref={ref} type="text" />
+  )
 }
+
+Input = React.forwardRef(Input);
+
+class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.pref = React.createRef(null);
+  }
+  componentDidMount(){
+    this.pref.current.focus();
+    console.log(this.pref)
+  }
+  render(){
+    return (
+      <div style={{marginTop:"10px",marginLeft:'10px'}}>
+        <Input abc="123" ref={this.pref} />
+      </div>
+    )
+  }
+}
+
 
 root.render(<App />);
 // If you want to start measuring performance in your app, pass a function
